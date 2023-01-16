eca6f292d8a040dd966d04102a724beccf7293f9
hazimaiman <aish22230105@gmail.com>
2023-01-16 16:18:02 +0800

    Add project files.

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+
+        public int Id { get; set; }
+        public String JokeQuestion { get; set; }
+
+        public String JokeAnswer { get; set;}
+
+        public Joke()
+        {
+
+        }
+    }
+}
